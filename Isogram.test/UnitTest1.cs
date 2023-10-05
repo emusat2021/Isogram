512f53f5bba65d60894b7291cfe8eee8d6c8a9d3
Eugenia Musat <eugenia.musat@nepa.com>
2023-10-05 21:18:18 +0200

    Add Models
    It makes  possible to use a object in test unit

--- Isogram.test/UnitTest1.cs
+++ Isogram.test/UnitTest1.cs
@@ -5,10 +5,23 @@ namespace Isogram.app;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Shoul_return_true_string_isogram()
     {
-        var repetitiveCharModel = new RepetitiveCharModel();
 
-        Assert.Equal("Isogra", repetitiveCharModel.Test("abcd"));
+        string input = "abcd";
+        var repetitiveCharModel = new RepetitiveCharModel();
+        var actual = repetitiveCharModel.FindIfIsogram(input);
+
+        Assert.Equal("Isogram", actual);
+    }
+    [Fact]
+    public void Shoul_return_error_string_not_isogram()
+    {
+
+        string input = "abcddcc";
+        var repetitiveCharModel = new RepetitiveCharModel();
+        var actual = repetitiveCharModel.FindIfIsogram(input);
+
+        Assert.Equal("NoIsogram", actual);
     }
 }
